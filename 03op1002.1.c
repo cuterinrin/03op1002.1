e24c96f002836abf59c1357337a63d53549bd28b
cuterinrin <yro7045@naver.com>
2018-10-02 15:59:34 +0900

    03op-3rd

--- 03op1002.1.c
+++ 03op1002.1.c
@@ -4,12 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int sec; 
+	int year; 
+	int result;
 	
-	printf("input second : ");
-	scanf("%d" , &sec);
 	
-	printf("%d sec. is %i : %i\n", sec, sec/60, sec%60);
+	printf("input : ");
+	scanf("%d" , &year);
+	
+	result=((year%4 ==0 && year%100 != 0)|| (year%400 == 0));
+	
+	printf("leap year? %d\n", result);
 
 	return 0;
 }
